ce56c97ce6829ad55d4d43b3a678e000ec953744
Calcfer <ucasl62@gmail.com>
2023-05-26 14:11:15 +1000

    adding hrefs

--- Javascript/HomePage.js
+++ Javascript/HomePage.js
@@ -8,11 +8,11 @@ let searchBg_open = document.getElementById("searchBg_open");
 
 
 function goHomePage() {
-    window.location.href = "../index.html"
+    window.location.href = "/book/index.html"
 }
 
 function goProductPage() {
-    window.location.href = "Html/product_page.html";
+    window.location.href = "/book/Html/product_page.html";
 }
 
 
