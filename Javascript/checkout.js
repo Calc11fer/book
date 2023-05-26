7e0d174a0d7b974b84bf23abe83d44f62e324ed1
Calcfer <ucasl62@gmail.com>
2023-05-26 20:58:36 +1000

    write functions for navbar links, changed few lines of code in css

--- Javascript/checkout.js
+++ Javascript/checkout.js
@@ -126,7 +126,37 @@ let checkout_shipping = document.getElementsByClassName("checkout_shipping");
 let shipping_method = document.getElementsByClassName("shipping_method");
 let paymentPage = document.getElementsByClassName("paymentPage");
 let shipMethod_checked = document.getElementsByClassName("shipMethod_checked");
+let navbar_links = document.getElementsByClassName("navbar_links");
 
+
+function returnToInfo() {
+    checkout[0].style.display = "block"
+    checkout_shipping[0].style.display = "none";
+
+    // change the style of the checkout navbar links
+    navbar_links[1].style.fontWeight = "500";
+    navbar_links[1].style.color = "#2F2F2F"
+    navbar_links[2].style.fontWeight = "normal";
+    navbar_links[2].style.color = "#4A4A4A";
+    navbar_links[3].style.fontWeight = "normal";
+    navbar_links[3].style.color = "#4A4A4A";
+}
+
+function returnToShip() {
+    paymentPage[1].style.display = "none";
+    paymentPage[0].style.display = "none";
+    shipping_method[0].style.display = "block";  
+    document.getElementById("shipping_standard").style.display = "none";
+    document.getElementById("shipping_express").style.display = "none"; 
+
+    // change the style of the checkout navbar links
+    navbar_links[2].style.fontWeight = "500";
+    navbar_links[2].style.color = "#2F2F2F"
+    navbar_links[3].style.fontWeight = "normal";
+    navbar_links[3].style.color = "#4A4A4A";
+    navbar_links[1].style.fontWeight = "normal";
+    navbar_links[1].style.color = "#4A4A4A";
+}
 
 function checked_1() {
     shipMethod_checked[0].style.backgroundColor = "#EDF2F8";
@@ -150,6 +180,15 @@ userInfo_form.addEventListener("submit", function(e) {
     e.preventDefault();
     checkout[0].style.display = "none"
     checkout_shipping[0].style.display = "block";
+
+    // change the style of the checkout navbar links
+    navbar_links[2].style.fontWeight = "500";
+    navbar_links[2].style.color = "#2F2F2F"
+    navbar_links[3].style.fontWeight = "normal";
+    navbar_links[3].style.color = "#4A4A4A";
+    navbar_links[1].style.fontWeight = "normal";
+    navbar_links[1].style.color = "#4A4A4A";
+    
     const emailNew = email.value;
     const countryNew = country.value;
     const lastNameNew = lastName.value;
@@ -168,6 +207,15 @@ shipping_form.addEventListener("submit", function(e) {
     shipping_method[0].style.display = "none";
     paymentPage[0].style.display = "flex";
     paymentPage[1].style.display = "block";
+
+    // change the style of the checkout navbar links
+    navbar_links[3].style.fontWeight = "500";
+    navbar_links[3].style.color = "#2F2F2F"
+    navbar_links[2].style.fontWeight = "normal";
+    navbar_links[2].style.color = "#4A4A4A";
+    navbar_links[1].style.fontWeight = "normal";
+    navbar_links[1].style.color = "#4A4A4A";
+    
     let shipMethod = document.querySelector('input[name="shipMethod"]:checked').value;
     if (shipMethod === "standard") {
         document.getElementById("shipping_standard").style.display = "block";
